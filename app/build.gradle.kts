2462494ece8557bd11dda870e14770af2e95163c
tmfrl1590 <tmfrl1590@gmail.com>
2024-05-18 15:39:44 +0900

    #31 build.gradle.kts 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
@@ -100,7 +100,7 @@ dependencies {
     // LiveData
     implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
 
-    implementation("com.google.android.gms:play-services-ads:23.0.0")
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
 
     // Sandwich
     implementation("com.github.skydoves:sandwich-retrofit:2.0.6")
