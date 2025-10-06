e23b92a4cf8fa90b999553f9691fb87ffffeb551
seulgi <tmfrl1590@gmail.com>
2025-10-06 21:17:53 +0900

    Release: 2.0.6 출시

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.system.traffic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 35
-        versionName = "2.0.5"
+        versionCode = 36
+        versionName = "2.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
