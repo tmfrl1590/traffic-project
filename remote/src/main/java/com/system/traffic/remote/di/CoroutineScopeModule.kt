7b96da40831277a7215e964556e50bc5b88eaa77
seulgi <tmfrl1590@gmail.com>
2026-08-03 13:48:51 +0900

    Fix: 임폴트 수정

--- remote/src/main/java/com/system/traffic/remote/di/CoroutineScopeModule.kt
+++ remote/src/main/java/com/system/traffic/remote/di/CoroutineScopeModule.kt
@@ -4,8 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Qualifier
-import jakarta.inject.Singleton
+import javax.inject.Qualifier
+import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
