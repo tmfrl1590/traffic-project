61bdfc301f1790a0e4d0687023b38114868fdeb8
seulgi <tmfrl1590@gmail.com>
2026-08-18 10:55:47 +0900

    Chore: GitHub Actions CI 및 ktlint(미사용 import 검사) 도입

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,5 +21,17 @@ plugins {
 
     id("com.google.gms.google-services") version "4.4.2" apply false
     alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.ktlint) apply false
+}
 
+// 모든 모듈에 ktlint 적용 (룰은 루트 .editorconfig에서 관리)
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        filter {
+            exclude { it.file.path.contains("/build/") }
+        }
+    }
 }
